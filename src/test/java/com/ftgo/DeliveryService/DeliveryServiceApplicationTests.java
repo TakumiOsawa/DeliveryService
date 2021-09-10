3f88630f2d6142d4d2f3a014f2ded24a148ad2e9
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-10 20:05:24 +0900

    Initial commit

---
+++ src/test/java/com/ftgo/DeliveryService/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftgo.DeliveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
